7cd172f05fda336c80150967ce2ad578a095c1a7
Aakif <aakifraj17@gmail.com>
2025-07-08 16:17:53 +0530

    Add Week 1 Python practice files
    Includes basics.py, variables_practice.py, and input_calc.py with input/output practice.

---
+++ input_calc.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter first number: "))
+
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_result}.")
---
+++ variables_practice.py
@@ -0,0 +1,5 @@
+name = "Aakif"
+age = 18
+city = "Indore"
+
+print(f"My name is {name}. I am {age} years old. I live in {city}. ")
